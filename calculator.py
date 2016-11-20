9a8797287a063da89d9f94680b63c8ee8d77ca09
Arturo Flores <Arturo Flores>
2016-11-20 02:53:35 -0600

    Fix for the problems on the file 'calculator.py' for the py2 challenge

--- calculator.py
+++ calculator.py
@@ -1,20 +1,24 @@
+
 """
 This class is used to return the sum of a list of integers.
 Notes:
-	it does not validates if the values are integers
-	there are easy ways to do this
-	there is also more pythonic ways to do this(currently working on them)
+  it does not validates if the values are integers
+  there are easy ways to do this.
+  there is also more pythonic ways to do this(currently working on them).
 """
 
 
 class CalculatorClass(object):
-	pass
-	"""
-	num_list: A list of integers.
-	sum_of_i: This variable has the elements summed for the final result
-	"""
-	def sum(self,num_list):
-		sum_of_i=0
-		for i in num_list:
-			sum_of_i+=i
-		return sum_of_i
+
+    """
+    sum: method to sum a list of values.
+    """
+    def sum(self, num_list):
+        """
+        num_list: A list of integers.
+        sum_of_i: This variable has the elements summed for the final result.
+        """
+        sum_of_i = 0
+        for i_co in num_list:
+            sum_of_i += i_co
+        return sum_of_i
