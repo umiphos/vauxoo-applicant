6ed21a375d907dae9447c410c78ce803b6a53a85
Arturo Flores <Arturo Flores>
2016-11-20 02:52:26 -0600

    Fix for the problems on the file 'primes.py' for the py1 challenge

--- primes.py
+++ primes.py
@@ -1,16 +1,24 @@
-class PrimeClass(object):
-	"""
-	This is a class to get if a given number is odd or not.
-	
-	Attributes:
-		
-	"""
-	def is_prime(self,num_int):
-		"""
-		num_int: An integer that is going to be processed and it will return if is or not an odd number.
-		"""
-		if (num_int % 2 == 0):
-			return True
-		else: 
-			return False
 
+"""
+PrimeClass class.
+"""
+
+
+class PrimeClass(object):
+    """
+    This is a class to get if a given number is odd or not.
+    Attributes:
+        is_prime:a function to get elements of a list sum up on a single
+        function.
+    """
+
+    def is_prime(self, num_int):
+        """
+        num_int: An integer that is going to be processed
+        """
+        if num_int < 2:
+            return False
+        for i_co in xrange(2, num_int):
+            if num_int % i_co == 0:
+                return False
+        return True
