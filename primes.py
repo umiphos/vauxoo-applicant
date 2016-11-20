ffa056307520288c8d3ac687bf85b6bdd46b9fd2
Arturo Flores <Arturo Flores>
2016-11-19 21:22:08 -0600

    'py1' on Vauxoo's technical test for python

--- primes.py
+++ primes.py
@@ -4,13 +4,18 @@ Your module documentation here
 
 
 class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+	"""
+	This is a class to get if a given number is odd or not.
+	
+	Attributes:
+		
+	"""
+	def is_prime(num_int):
+		"""
+		num_int: An integer that is going to be processed and it will return if is or not an odd number.
+		"""
+		if (num_int % 2 == 0):
+			return True
+		else: 
+			return False
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
